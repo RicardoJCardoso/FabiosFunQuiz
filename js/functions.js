9fb8b5ddf8d77ff588909bae341947e8b94d3212
Ricardo <ricardo.cardoso1998@outlook.com>
2023-07-25 23:41:42 +0100

    changes

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 disabledAnswers = [];
 Questions = {}
 Answers = {}
+AuthorName = "";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAUY6f4vJkgxrsgfXnUq23-xvPvkmJ5tJs",
@@ -20,13 +21,24 @@ const database = firebase.database();
 
 
 //writeQuestions();
-readQuestions();
-readAnswers();
+try {
+    readQuestions();
+} catch (error) {
+    console.log(error);
+
+}
+
+try {
+    readAnswers();
+} catch (error) {
+    console.log(error);
+
+}
 
 function writeQuestions() {
     // Write data to the database
     database.ref('Questions').set({
-        "1" : 'What is your favorite Color?',
+        "1": 'What is your favorite Color?',
     });
 }
 
@@ -76,7 +88,7 @@ function readAnswers() {
                 a.innerHTML = `<span>${element}</span> 
                     <button class="btn btn-primary" onclick="removeItemAnswer(this)">Remove</button>`;
                 editableList.appendChild(a);
-                newItemInput.value = '';            
+                newItemInput.value = '';
             });
         })
         .catch(error => {
@@ -103,15 +115,15 @@ function addItem() {
                     <button class="btn btn-primary" onclick="removeItem(this)">Remove</button>`;
     editableList.appendChild(a);
     newItemInput.value = '';
-    if(Questions === null){
+    if (Questions === null) {
         Questions = {}
         Questions[0] = newItemText;
-    }else{
-        
+    } else {
+
         var count = Object.keys(Questions).length;
         Questions[count] = newItemText;
     }
-    
+
     database.ref().update({
         Questions
     });
@@ -122,11 +134,11 @@ function addItem() {
 function removeItem(removeButton) {
 
     const li = removeButton.parentElement;
-    let value= li.querySelector('span').textContent;
+    let value = li.querySelector('span').textContent;
 
     const index = Questions.findIndex((element) => element === value);
 
-    Questions.splice(index,1)
+    Questions.splice(index, 1)
 
     database.ref().update({
         Questions
@@ -152,15 +164,15 @@ function addItemAnswer() {
                     <button class="btn btn-primary" onclick="removeItemAnswer(this)">Remove</button>`;
     editableList.appendChild(a);
     newItemInput.value = '';
-    if(Answers === null){
+    if (Answers === null) {
         Answers = {}
         Answers[0] = newItemText;
-    }else{
-        
+    } else {
+
         var count = Object.keys(Answers).length;
         Answers[count] = newItemText;
     }
-    
+
     database.ref().update({
         Answers
     });
@@ -171,11 +183,11 @@ function addItemAnswer() {
 // Function to remove an item from the list
 function removeItemAnswer(removeButton) {
     const li = removeButton.parentElement;
-    let value= li.querySelector('span').textContent;
+    let value = li.querySelector('span').textContent;
 
     const index = Answers.findIndex((element) => element === value);
 
-    Answers.splice(index,1)
+    Answers.splice(index, 1)
 
     database.ref().update({
         Answers
@@ -189,6 +201,8 @@ function removeItemAnswer(removeButton) {
 
 
 function showQuiz() {
+    AuthorName = document.getElementById('exampleInputEmail1').textContent;
+    console.log(document.getElementById('exampleInputEmail1'));
     document.getElementById('formID').classList.add('d-none');
     document.getElementById('quizID').classList.remove('d-none');
 }
@@ -213,17 +227,79 @@ function checkAnswer(questionId) {
             disableSelectedAnswer('q3', selectedAnswer);
             document.getElementById('question2').classList.add('d-none');
             document.getElementById('question3').classList.remove('d-none');
+            
         } else if (questionId === 'q3') {
-            disableSelectedAnswer('q3', selectedAnswer);
+            disableSelectedAnswer('q4', selectedAnswer);
+            document.getElementById('question3').classList.add('d-none');
+            document.getElementById('question4').classList.remove('d-none');
+            
+        } else if (questionId === 'q4') {
+            disableSelectedAnswer('q5', selectedAnswer);
+            document.getElementById('question4').classList.add('d-none');
+            document.getElementById('question5').classList.remove('d-none');
+            
+        } else if (questionId === 'q5') {
+            disableSelectedAnswer('q6', selectedAnswer);
+            document.getElementById('question5').classList.add('d-none');
+            document.getElementById('question6').classList.remove('d-none');
+            
+        } else if (questionId === 'q6') {
+            disableSelectedAnswer('q7', selectedAnswer);
+            document.getElementById('question6').classList.add('d-none');
+            document.getElementById('question7').classList.remove('d-none');
+            
+        } else if (questionId === 'q7') {
+            disableSelectedAnswer('q8', selectedAnswer);
+            document.getElementById('question7').classList.add('d-none');
+            document.getElementById('question8').classList.remove('d-none');
+            
+        } else if (questionId === 'q8') {
+            disableSelectedAnswer('q9', selectedAnswer);
+            document.getElementById('question8').classList.add('d-none');
+            document.getElementById('question9').classList.remove('d-none');
+            
+        } else if (questionId === 'q9') {
+            disableSelectedAnswer('q10', selectedAnswer);
+            document.getElementById('question9').classList.add('d-none');
+            document.getElementById('question10').classList.remove('d-none');
+            
+        } 
+        else if (questionId === 'q10') {
+            disableSelectedAnswer('q10', selectedAnswer);
             // Perform final calculations or show results
             // For now, let's just display the selected answers
-            var selectedAnswers = {
-                q1: document.querySelector('input[name="q1"]:checked').value,
-                q2: document.querySelector('input[name="q2"]:checked').value,
-                q3: document.querySelector('input[name="q3"]:checked').value
-            };
 
-            
+            var q1 = document.getElementById('question1').getElementsByTagName('h2')[0].outerText;
+            var q2 = document.getElementById('question2').getElementsByTagName('h2')[0].outerText;
+            var q3 = document.getElementById('question3').getElementsByTagName('h2')[0].outerText;
+            var q4 = document.getElementById('question4').getElementsByTagName('h2')[0].outerText;
+            var q5 = document.getElementById('question5').getElementsByTagName('h2')[0].outerText;
+            var q6 = document.getElementById('question6').getElementsByTagName('h2')[0].outerText;
+            var q7 = document.getElementById('question7').getElementsByTagName('h2')[0].outerText;
+            var q8 = document.getElementById('question8').getElementsByTagName('h2')[0].outerText;
+            var q9 = document.getElementById('question9').getElementsByTagName('h2')[0].outerText;
+            var q10 = document.getElementById('question10').getElementsByTagName('h2')[0].outerText;
+
+            var selectedAnswers = {
+                Teste : {
+                    [q1]: document.querySelector('input[name="q1"]:checked').value,
+                    [q2]: document.querySelector('input[name="q2"]:checked').value,
+                    [q3]: document.querySelector('input[name="q3"]:checked').value,
+                    [q4]: document.querySelector('input[name="q4"]:checked').value,
+                    [q5]: document.querySelector('input[name="q5"]:checked').value,
+                    [q6]: document.querySelector('input[name="q6"]:checked').value,
+                    [q7]: document.querySelector('input[name="q7"]:checked').value,
+                    [q8]: document.querySelector('input[name="q8"]:checked').value,
+                    [q9]: document.querySelector('input[name="q9"]:checked').value,
+                    [q10]: document.querySelector('input[name="q10"]:checked').value
+                }
+            };
+  
+            console.log(selectedAnswers);
+
+            submitAnswers(selectedAnswers);
+
+
         }
     } else {
         alert('Please select an answer.');
@@ -243,3 +319,221 @@ function disableSelectedAnswer(questionId, selectedAnswer) {
         }
     }
 }
+
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateNonRepeatingRandomNumbers(count, min, max) {
+    if (max - min + 1 < count) {
+        console.log("Cannot generate non-repeating random numbers with the given range and count.");
+        return [];
+    }
+
+    const numbers = [];
+    while (numbers.length < count) {
+        const randomNumber = getRandomNumber(min, max);
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
+
+    return numbers;
+}
+
+
+function loadQuestionsQ() {
+    let questions;
+    database.ref('Questions').once('value')
+        .then(snapshot => {
+            const data = snapshot.val();
+            questions = data;
+            let numbers = generateNonRepeatingRandomNumbers(10, 0, 9);
+            let count = 1;
+            numbers.forEach(element => {
+                addQuestionHTML(data[element],count);
+                count++;
+            });
+        }).catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+function addQuestionHTML(question, qID) {
+
+    console.log(question);
+
+    const quiz = document.getElementById("quizID");
+
+    let div = "";
+
+    database.ref('Answers').once('value')
+    .then(snapshot => {
+        const data = snapshot.val();
+        anwswers = data;
+
+        if (qID === 1) {
+            div = '<div id="question' + qID +'"> ' +
+               '<h2>' + question + '</h2> ' +
+               '<p>Select the correct answer:</p>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               'value="' + anwswers[0] + '">'+ anwswers[0] +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[1] + '">' + anwswers[1]  +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[2] + '">' + anwswers[2] +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[3] + '">' + anwswers[3] +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[4] + '">' + anwswers[4] +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[5] + '">' + anwswers[5] +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[6] + '">' + anwswers[6] +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[7] + '">' + anwswers[7] +
+               '</div>' +
+
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[8] + '">' + anwswers[8] +
+               '</div>' +
+               '<div class="input-group-text">' +
+               '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+               '        value="' + anwswers[9] + '">' + anwswers[9] +
+               '</div>' +
+               '<button onclick="checkAnswer(\'q1\')" class="btn btn-secondary">Next</button>' +
+               '</div>';
+   
+       }else if(qID === 10){
+           div = '<div id="question'+ qID + '" class="d-none"> ' +
+           '<h2>' + question + '</h2> ' +
+           '<p>Select the correct answer:</p>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           'value="' + anwswers[0] + '">'+ anwswers[0] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[1] + '">' + anwswers[1]  +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[2] + '">' + anwswers[2] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[3] + '">' + anwswers[3] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[4] + '">' + anwswers[4] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[5] + '">' + anwswers[5] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[6] + '">' + anwswers[6] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[7] + '">' + anwswers[7] +
+           '</div>' +
+
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[8] + '">' + anwswers[8] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[9] + '">' + anwswers[9] +
+           '</div>' +
+           '<button onclick="checkAnswer(\'q'+ qID +'\')" class="btn btn-success">Submit</button>' + 
+           '</div>';
+       }else{
+            div = '<div id="question'+ qID + '" class="d-none"> ' +
+           '<h2>' + question + '</h2> ' +
+           '<p>Select the correct answer:</p>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           'value="' + anwswers[0] + '">'+ anwswers[0] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[1] + '">' + anwswers[1]  +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[2] + '">' + anwswers[2] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[3] + '">' + anwswers[3] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[4] + '">' + anwswers[4] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[5] + '">' + anwswers[5] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[6] + '">' + anwswers[6] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[7] + '">' + anwswers[7] +
+           '</div>' +
+
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[8] + '">' + anwswers[8] +
+           '</div>' +
+           '<div class="input-group-text">' +
+           '    <input type="radio" aria-label="Radio button for following text input" name="q' + qID +'"' +
+           '        value="' + anwswers[9] + '">' + anwswers[9] +
+           '</div>' +
+           '<button onclick="checkAnswer(\'q'+ qID +'\')" class="btn btn-secondary">Next</button>' +
+           '</div>';
+       }
+       quiz.innerHTML = quiz.innerHTML + div;
+
+    }).catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
+
+}
+
+function submitAnswers(selectedAnswers){
+
+    database.ref().update({
+        selectedAnswers
+    });
+
+}
+
+
+
+
+loadQuestionsQ();
